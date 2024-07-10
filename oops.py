d072159ed894e2adb90ec15feeed38db28bba533
pgspraveen <146072007+pgspraveen@users.noreply.github.com>
2024-07-10 22:56:04 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -281,7 +281,7 @@ print(obj.c)
 print(obj.d)
 print(obj.x+obj.y)'''
 #operator overloading
-class A:
+'''class A:
     def __init__(self,x):
         self.x=x
     def __add__(self,other):
@@ -296,7 +296,7 @@ obj1=A(10)
 obj2=A(20)
 print(obj1.x+obj2.x)
 print(obj1<obj2)
-
+'''
 '''class A:
     def __init__(self,x,y):
         self.x=x
